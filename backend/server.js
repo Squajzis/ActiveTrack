49b71f0b3783cc501c750a3a05bf8536f412c9e6
Squajzis <126024795+Squajzis@users.noreply.github.com>
2025-05-11 18:19:07 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const connectDB = require('./src/config/db'); 
-const errorMiddleware = require('../middleware/errorMiddleware');
+const errorMiddleware = require('./middleware/errorMiddleware');
 
 const activityRoutes = require('./src/routes/activityRoutes');
 const noteRoutes     = require('./src/routes/noteRoutes');
